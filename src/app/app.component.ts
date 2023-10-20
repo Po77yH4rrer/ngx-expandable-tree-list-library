9390d8a7c8e5e1278d777d3f1c010814f01593d3
paulvaehning <paul.vaehning@seracom.de>
2023-10-20 16:47:00 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-etl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-expandable-tree-list-library';
+}
